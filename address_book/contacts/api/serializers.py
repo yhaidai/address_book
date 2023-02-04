892014b4b96402cb5873b89a91e5c8f63405494a
Yaroslav Haidai <uneeeeeeex@gmail.com>
2023-02-04 20:38:03 +0100

    feat: Use slug uuid field for relationships instead of hyperlinks

--- address_book/contacts/api/serializers.py
+++ address_book/contacts/api/serializers.py
@@ -3,37 +3,35 @@ from rest_framework import serializers
 from ..models import Contact, ContactGroup
 
 
-class ContactSerializer(serializers.HyperlinkedModelSerializer):
+class ContactSerializer(serializers.ModelSerializer):
     """
-    Hyperlinked (de)serializer class for contacts.
+    Serializer class for contacts.
 
     User field is automatically set to current user (based on request context) during deserialization.
     """
 
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    contact_groups = serializers.SlugRelatedField(
+        queryset=ContactGroup.objects.all(), slug_field="uuid", many=True, required=False,
+    )
 
     class Meta:
         model = Contact
-        fields = ("first_name", "last_name", "email", "phone_number", "url", "contact_groups", "user",)
-        extra_kwargs = {
-            "url": {"view_name": "api:contact_detail", "lookup_field": "uuid"},
-            "contact_groups": {"view_name": "api:contact_group_detail", "lookup_field": "uuid"},
-        }
+        fields = ("first_name", "last_name", "email", "phone_number", "contact_groups", "user", "uuid",)
 
 
-class ContactGroupSerializer(serializers.HyperlinkedModelSerializer):
+class ContactGroupSerializer(serializers.ModelSerializer):
     """
-    Hyperlinked (de)serializer class for contact groups.
+    Serializer class for contact groups.
 
     User field is automatically set to current user (based on request context) during deserialization.
     """
 
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    contacts = serializers.SlugRelatedField(
+        queryset=Contact.objects.all(), slug_field="uuid", many=True, required=False,
+    )
 
     class Meta:
         model = ContactGroup
-        fields = ("name", "url", "contacts", "user",)
-        extra_kwargs = {
-            "url": {"view_name": "api:contact_detail", "lookup_field": "uuid"},
-            "contacts": {"view_name": "api:contact_detail", "lookup_field": "uuid"},
-        }
+        fields = ("name", "contacts", "user", "uuid",)
