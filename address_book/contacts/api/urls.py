6fb8ddfe82c16d62174ecd051ea6a7e3ee530e47
Yaroslav Haidai <uneeeeeeex@gmail.com>
2023-02-05 01:00:46 +0100

    feat: "add contact to group", "delete contact from group", "list contacts within group"

--- address_book/contacts/api/urls.py
+++ address_book/contacts/api/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
 
 from address_book.contacts.api.views import (
-    ContactDetailView, ContactListView, ContactGroupDetailView, ContactGroupListView,
+    ContactDetailView, ContactListView, ContactGroupDetailView, ContactGroupListView, ContactGroupContactListView,
+    ContactGroupContactDetailView,
 )
 
 urlpatterns = [
     path("contacts/", ContactListView.as_view(), name="contact_list"),
-    path("contact/<uuid:uuid>", ContactDetailView.as_view(), name="contact_detail"),
+    path("contacts/<uuid:uuid>", ContactDetailView.as_view(), name="contact_detail"),
     path("contact_groups/", ContactGroupListView.as_view(), name="contact_group_list"),
-    path("contact_group/<uuid:uuid>", ContactGroupDetailView.as_view(), name="contact_group_detail"),
+    path("contact_groups/<uuid:uuid>", ContactGroupDetailView.as_view(), name="contact_group_detail"),
+    path(
+        "contact_groups/<uuid:contact_group_uuid>/contacts",
+        ContactGroupContactListView.as_view(),
+        name="contact_group_contact_list",
+    ),
+    path(
+        "contact_groups/<uuid:contact_group_uuid>/contacts/<uuid:contact_uuid>",
+        ContactGroupContactDetailView.as_view(),
+        name="contact_group_contact_detail",
+    ),
 ]
--- address_book/contacts/api/views.py
+++ address_book/contacts/api/views.py
@@ -1,7 +1,14 @@
-from django.db.models import QuerySet
-from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
+from uuid import UUID
 
-from .serializers import ContactSerializer, ContactGroupSerializer
+from django.db.models import QuerySet, Manager
+from django.http import Http404
+from rest_framework import status
+from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import ContactSerializer, ContactGroupSerializer, ContactGroupContactSerializer
 from ..models import Contact, ContactGroup
 
 
@@ -41,3 +48,80 @@ class ContactGroupListView(ListCreateAPIView):
         """Filter contact groups on the current user and prefetch related `contacts` to avoid N+1 problem."""
         user = self.request.user
         return ContactGroup.objects.filter(user=user).prefetch_related("contacts")
+
+
+class ContactGroupContactDetailView(APIView):
+    """View for removing a contact from contact group by its UUID."""
+    def delete(self, request: Request, contact_group_uuid: UUID, contact_uuid: UUID) -> Response:
+        """
+        Remove a contact from a group.
+
+        :return: 404 NOT FOUND, error msg - if there is no contact/group with given UUID.
+                 204 NO CONTENT - if the contact has been successfully removed from the group.
+        """
+        try:
+            contact: Contact = Contact.objects.get(uuid=contact_uuid)
+        except Contact.DoesNotExist:
+            return Response(f"Contact with UUID '{contact_uuid}' does not exist", status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            contact_group: ContactGroup = ContactGroup.objects.get(uuid=contact_group_uuid)
+        except ContactGroup.DoesNotExist:
+            return Response(
+                f"ContactGroup with UUID '{contact_group_uuid}' does not exist", status=status.HTTP_404_NOT_FOUND,
+            )
+
+        contact_group.contacts.remove(contact)
+        return Response("", status=status.HTTP_204_NO_CONTENT)
+
+
+class ContactGroupContactListView(ListCreateAPIView):
+    """View for adding existing contact to a group and listing all contacts within the group."""
+
+    serializer_class = ContactGroupContactSerializer
+
+    def get_queryset(self) -> QuerySet[Contact]:
+        """
+        Filter contacts on the current user and contact group and prefetch related `contact_groups` to avoid N+1
+        problem.
+
+        :raise Http404: if there is no contact group with given UUID
+        """
+        user = self.request.user
+        contact_group_uuid = self.kwargs["contact_group_uuid"]
+
+        try:
+            contact_group: ContactGroup = ContactGroup.objects.get(user=user, uuid=contact_group_uuid)
+        except ContactGroup.DoesNotExist as error:
+            raise Http404(f"ContactGroup with UUID '{contact_group_uuid}' does not exist") from error
+
+        return contact_group.contacts.prefetch_related("contact_groups")
+
+    def create(self, request: Request, contact_group_uuid: UUID) -> Response:
+        """
+        Add an existing contact to the contact group.
+
+        :return Response: 404 NOT FOUND, error msg - if there is no contact/group with given UUID
+                          303 SEE OTHER, serialized contact - if this contact is already in the group
+                          200 OK, serialized contact - if the contact has been successfully added to the group
+        """
+        contact_uuid = request.data["uuid"]
+        try:
+            contact: Contact = Contact.objects.get(uuid=contact_uuid)
+        except Contact.DoesNotExist:
+            return Response(f"Contact with UUID '{contact_uuid}' does not exist", status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            contact_group: ContactGroup = ContactGroup.objects.get(uuid=contact_group_uuid)
+        except ContactGroup.DoesNotExist:
+            return Response(
+                f"ContactGroup with UUID '{contact_group_uuid}' does not exist", status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = ContactGroupContactSerializer(contact, context={"request": request})
+
+        if contact in contact_group.contacts.all():
+            return Response(serializer.data, status=status.HTTP_303_SEE_OTHER)
+
+        contact_group.contacts.add(contact)
+        return Response(serializer.data, status=status.HTTP_200_OK)
