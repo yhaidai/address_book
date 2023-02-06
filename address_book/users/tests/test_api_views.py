45e61fdeb469b6b2b86a4f8d07d3dfece8e6a9e4
Yaroslav Haidai <uneeeeeeex@gmail.com>
2023-02-06 17:25:50 +0100

    refactor: Replace RequestFactory with APIRequestFactory

--- address_book/users/tests/test_api_views.py
+++ address_book/users/tests/test_api_views.py
@@ -1,25 +1,27 @@
-from django.test import RequestFactory
+from rest_framework.test import APIRequestFactory
 
 from address_book.users.api.views import UserViewSet
 from address_book.users.models import User
 
 
 class TestUserViewSet:
-    def test_get_queryset(self, user: User, rf: RequestFactory):
+    def test_get_queryset(self, user: User):
+        request_factory = APIRequestFactory()
         view = UserViewSet()
-        request = rf.get("/fake-url/")
+        request = request_factory.get("/fake-url/")
         request.user = user
 
-        view.request = request  # type: ignore
+        view.request = request
 
         assert user in view.get_queryset()
 
-    def test_me(self, user: User, rf: RequestFactory):
+    def test_me(self, user: User):
+        request_factory = APIRequestFactory()
         view = UserViewSet()
-        request = rf.get("/fake-url/")
+        request = request_factory.get("/fake-url/")
         request.user = user
 
-        view.request = request  # type: ignore
+        view.request = request
 
         response = view.me(request)  # type: ignore
 
