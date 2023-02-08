085505ba663b5c5968da890d6f7e9423eb7df71a
Yaroslav Haidai <uneeeeeeex@gmail.com>
2023-02-08 18:16:15 +0100

    feat: Add trailing slashes to urls

--- address_book/contacts/api/urls.py
+++ address_book/contacts/api/urls.py
@@ -12,18 +12,18 @@ from address_book.contacts.api.views import (
 
 urlpatterns = [
     path("contacts/", ContactListView.as_view(), name="contact_list"),
-    path("contacts/<uuid:uuid>", ContactDetailView.as_view(), name="contact_detail"),
+    path("contacts/<uuid:uuid>/", ContactDetailView.as_view(), name="contact_detail"),
     path("contact_groups/", ContactGroupListView.as_view(), name="contact_group_list"),
-    path("contact_groups/<uuid:uuid>", ContactGroupDetailView.as_view(), name="contact_group_detail"),
+    path("contact_groups/<uuid:uuid>/", ContactGroupDetailView.as_view(), name="contact_group_detail"),
     path(
-        "contact_groups/<uuid:contact_group_uuid>/contacts",
+        "contact_groups/<uuid:contact_group_uuid>/contacts/",
         ContactGroupAddListContactsView.as_view(),
         name="contact_group_add_list_contacts",
     ),
     path(
-        "contact_groups/<uuid:contact_group_uuid>/contacts/<uuid:contact_uuid>",
+        "contact_groups/<uuid:contact_group_uuid>/contacts/<uuid:contact_uuid>/",
         ContactGroupRemoveContactView.as_view(),
         name="contact_group_remove_contact",
     ),
-    path("contact_groups/search", ContactGroupSearch.as_view(), name="contact_group_search")
+    path("contact_groups/search/", ContactGroupSearch.as_view(), name="contact_group_search")
 ]
