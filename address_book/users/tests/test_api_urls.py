d6123469d6aef4150fcc2817e9e0dcd466adc376
Yaroslav Haidai <uneeeeeeex@gmail.com>
2023-02-10 11:05:25 +0100

    test: Refactor api urls tests for users

--- address_book/users/tests/test_api_urls.py
+++ address_book/users/tests/test_api_urls.py
@@ -1,21 +1,14 @@
-from django.urls import resolve, reverse
-
+from address_book.conftest import assert_view_name_matches_url
 from address_book.users.models import User
 
 
 def test_user_detail(user: User):
-    assert (
-        reverse("api:user-detail", kwargs={"username": user.username})
-        == f"/api/users/{user.username}/"
-    )
-    assert resolve(f"/api/users/{user.username}/").view_name == "api:user-detail"
+    assert_view_name_matches_url("api:user-detail", "/api/users/{username}/", username=user.username)
 
 
 def test_user_list():
-    assert reverse("api:user-list") == "/api/users/"
-    assert resolve("/api/users/").view_name == "api:user-list"
+    assert_view_name_matches_url("api:user-list", "/api/users/")
 
 
 def test_user_me():
-    assert reverse("api:user-me") == "/api/users/me/"
-    assert resolve("/api/users/me/").view_name == "api:user-me"
+    assert_view_name_matches_url("api:user-me", "/api/users/me/")
