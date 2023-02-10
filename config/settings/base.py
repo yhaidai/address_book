c8cab6694d86674babf1622e5255e64e3ce7e3b8
Yaroslav Haidai <uneeeeeeex@gmail.com>
2023-02-10 11:06:29 +0100

    fix: fix local run without Docker

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,7 +8,9 @@ import environ
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # address_book/
 APPS_DIR = BASE_DIR / "address_book"
-env = environ.Env()
+env = environ.Env(
+    USE_DOCKER=(str, "no"),
+)
 
 READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
 if READ_DOT_ENV_FILE:
